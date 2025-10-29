b27d1a031c555465cd5280020ca2c8b25b635d3d
Dory <pemegomachado@gmail.com>
2025-10-29 18:02:49 +0100

    Improved donate.js a ton

--- res/donate.js
+++ res/donate.js
@@ -3,10 +3,8 @@ const donationInput = document.getElementById('donateAmount');
 let donationValue = parseInt(donationInput.value) || 10;
 let setPrice = -1;
 donationInput.addEventListener('input', async e => {
-    setLoading(true);
     donationValue = parseInt(e.target.value) || 10;
-    await initialize();
-    setLoading(false);
+    initialize();
 });
 let currency = 'eur';
 let elements;
@@ -15,30 +13,29 @@ initialize();
 document.querySelector("#payment-form").addEventListener("submit", handleSubmit);
 
 // Fetches a payment intent and captures the client secret
-async function initialize() {
-    setPrice = donationValue;
-    const response = await fetch("/create-payment-intent", {
+function initialize() {
+    setLoading(true);
+    fetch("/create-payment-intent", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ donationValue: donationValue * 10, currency: currency })
-    });
-    const { clientSecret } = await response.json();
-
-    const appearance = { theme: 'stripe' };
-    elements = stripe.elements({ appearance, clientSecret });
-
-    const paymentElement = elements.create("payment", {
-        layout: "accordion"
-    });
-    paymentElement.mount("#payment-element");
+    }).then(res => res.json()).catch(err => console.log(err)).then(data => {
+        const clientSecret = data.clientSecret;
+        const appearance = {
+            theme: 'stripe',
+        };
+        elements = stripe.elements({ appearance, clientSecret });
+        setPrice = donationValue;
+        const paymentElement = elements.create("payment");
+        paymentElement.mount("#payment-element");
+        setLoading(false);
+    }).catch(err => console.log(err));
 }
 
 async function handleSubmit(e) {
     if (setPrice !== donationValue) return;
-
     e.preventDefault();
     setLoading(true);
-
     const { error } = await stripe.confirmPayment({
         elements,
         confirmParams: {
@@ -46,24 +43,17 @@ async function handleSubmit(e) {
             return_url: "https://your-website.com/order/complete",
         },
     });
-
-    if (error.type === "card_error" || error.type === "validation_error") showMessage(error.message);
-    else showMessage("An unexpected error occurred.");
-
+    showMessage((error.type in ["card_error", "validation_error"]) ? error.message :
+                                                                                 "An unexpected error occurred.");
     setLoading(false);
 }
 
 // ------- UI helpers -------
 function showMessage(messageText) {
     const messageContainer = document.querySelector("#payment-message");
-
-    messageContainer.classList.remove("hidden");
     messageContainer.textContent = messageText;
-
-    setTimeout(function () {
-        messageContainer.classList.add("hidden");
-        messageContainer.textContent = "";
-    }, 4000);
+    messageContainer.classList.remove("hidden");
+    setTimeout(() => messageContainer.classList.add("hidden"), 5000);
 }
 
 // Show a spinner on payment submission
