a1c6986c57a14f802ea9516bd9be4811963af019
Dory <pemegomachado@gmail.com>
2025-10-30 16:48:38 +0100

    Improved server.js a little bit

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
 app.use(express.static("./"));
 app.use(express.json());
-
 app.post("/create-payment-intent", async (req, res) => {
     // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
         amount: req.body.donationValue,
         currency: req.body.currency
     });
-
     res.send({ clientSecret: paymentIntent.client_secret, });
 });
-
-
 app.listen(4242, () => console.log("Node server listening on port 4242!"));
